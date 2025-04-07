c4c4df523dd5fbf97469fbfa8eb0d9c6688863f1
Heather Diamond <baishenkokv@gmail.com>
2025-04-07 22:58:18 +0300

    сделала меню действий и вынесла добавление отдельной функцией

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+def show_start_menu(students):
+    try:
+        number_of_action = int(input(f"""Доступные операции:
+1. показать информацию обо всех студентах.
+2. добавить нового студента.
+3. удалить студента с самым низким средним баллом.
+4. выход.
+Введите номер операции: """))
+        match number_of_action:
+            case 1:
+                students_information(students)
+            case 2:
+                add_new_student(students)
+            case 3:
+                del_student_with_lowest_grades(students)
+            case 4:
+                return
+            case _:
+                print("Указанный номер не соответвествует ни одной операци.")
+    except ValueError:
+        print("Ошибка: Не верный формат данных!")
+    finally:
+        print('Программа завершила работу.')
+    return
+
+
 def students_information(students):
+    print("\n")
     for s in students:
         print(f"""Cтудент: {s["name"]}
 Средний балл: {round(sum(s["grades"]) / len(s["grades"]), 2)}
 Статус: {"Не успешен" if round(sum(s["grades"]) / len(s["grades"]), 2) < 75 else "Успешен"}""")
+    print(f"""Средний балл по всем студентам: {average_ball_of_all_students(students)}
+    """)
+    show_start_menu(students)
     return
 
 
@@ -14,9 +44,25 @@ def average_ball_of_all_students(students):
     return round(result, 2)
 
 
+def add_new_student(students):
+    name_of_student = input(f"""
+Введите имя студента: """)
+    grades_of_student = input("Введите оценки через пробелы: ")
+    grades_of_student_list = [int(num) for num in grades_of_student.split()]
+    students.append({"name": name_of_student, "grades": grades_of_student_list})
+    print(f"""Средний балл по всем студентам: {average_ball_of_all_students(students)}
+        """)
+    show_start_menu(students)
+    return
+
+
 def del_student_with_lowest_grades(students):
     weakest_student = min(students, key=lambda s: sum(s["grades"]))
     students.remove(weakest_student)
+    print(f"\nСтудент с самым низким баллом удален.")
+    print(f"""Средний балл по всем студентам: {average_ball_of_all_students(students)}
+            """)
+    show_start_menu(students)
     return
 
 
@@ -27,16 +73,5 @@ students = [
     {"name": "Draco", "grades": [60, 75, 70]}
 ]
 
-students_information(students)
 
-print(f"""Средний балл по всем студентам: {average_ball_of_all_students(students)}""")
-
-students.append({"name": "Joe", "grades": [82, 73, 88]})
-
-print(f"""Средний балл по всем студентам после добавления студентки Joe [82, 73, 88]: {average_ball_of_all_students(students)}""")
-
-del_student_with_lowest_grades(students)
-
-print(f"""Средний балл по всем студентам после удаления слабейшего: {average_ball_of_all_students(students)}""")
-
-
+show_start_menu(students)
